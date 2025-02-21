53e89cd29bf4f0a7a2407549c5daa8896ccbf2a8
Behinding978 <156109664+BehindingTheRealOne@users.noreply.github.com>
2025-02-21 12:06:26 -0500

    Update script.js

--- teacher/script.js
+++ teacher/script.js
@@ -3,7 +3,7 @@ document.addEventListener('keydown', function(event) {
     // Open the unblocked games site in a new about:blank tab/window
     let newWindow = window.open('about:blank', '_blank');
     if (newWindow) {
-      newWindow.document.write('<!DOCTYPE html><html><head><meta charset="UTF-8"><title>Home - Google Drive</title><link rel="icon" href="https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png"><style>body { margin: 0; }</style></head><body><iframe src="https://behindingtherealone.github.io/testing123/" style="width: 100%; height: 100vh; border: none;"></iframe></body></html>');
+      newWindow.document.write('<!DOCTYPE html><html><head><meta charset="UTF-8"><title>Home - Google Drive</title><link rel="icon" href="https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png"><style>body { margin: 0; }</style></head><body><iframe src="https://behindingtherealone.github.io/fakeguardian/.hidden/aboutblanklauncher.html" style="width: 100%; height: 100vh; border: none;"></iframe></body></html>');
       newWindow.document.close();
     } else {
       alert('Please allow pop-ups for this site to open the unblocked games.');
